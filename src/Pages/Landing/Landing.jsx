c9e56b0d85af1962072caf521f137782d31f27e7
LuisSigala <a6519150031@utch.edu.mx>
2021-06-29 22:24:27 -0600

    Add db.json file and configuring landing page

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-
+import "./Landing.css"
 
 class Landing extends React.Component{
     render(){
         return(
             <React.Fragment>
-                
+                <div className="background">
+                    <div>
+                        <div className="position">
+                            <br/><br/><br/><br/><br/><br/>
+                            <center className="colocation">
+                                <h1 className="title">Welcome</h1>
+                                <button className="button">Save the world!</button>
+                            </center>
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
