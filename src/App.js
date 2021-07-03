3d4fcc454b0b3a202fe922f3e9c0929e5f4ecb8e
LuisSigala <a6519150031@utch.edu.mx>
2021-07-02 22:13:22 -0600

    Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react' 
 import{ BrowserRouter, Route, Switch} from "react-router-dom"
 import Landing from './Pages/Landing/Landing';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/Login" component={Login}></Route>
         </Switch>
     </BrowserRouter>
   );
