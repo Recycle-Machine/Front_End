faf9e29d4aa73a2508b064a428252fb2f82ea034
LuisSigala <a6519150031@utch.edu.mx>
2021-07-17 19:11:16 -0600

    Landing connection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react' 
 import{ BrowserRouter, Route, Switch} from "react-router-dom"
-import Landing from './Pages/Landing/Landing';
+import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
+import Landing from './Pages/Landing/Landing';
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/" component={Main}></Route>
           <Route exact path="/Login" component={Login}></Route>
+          <Route exact path="/Landing" component={Landing}></Route>
         </Switch>
     </BrowserRouter>
   );
