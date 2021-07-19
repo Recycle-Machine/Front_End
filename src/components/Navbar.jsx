3f48211c7dbc30d3a37d1829dca4abd845d9d6aa
LuisSigala <a6519150031@utch.edu.mx>
2021-07-19 09:49:19 -0600

    Navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
 import React from "react"
-import "./styles/navbar.css"
-import logo from "../img/leaflogo.png"
-import offbutton from "../img/off.png"
+import "./css's/Navbar.css"
 
 class Navbar extends React.Component{
     render(){
         return(
             <React.Fragment>
             <div className="navbar">
-                <img src={logo} alt="leaflogo" id="logo" />
-                <img src={offbutton} alt="off" id="off"/>
+                <img src="https://www.dropbox.com/s/kiv2tlv76m5fond/leaflogo.png?raw=1" alt="leaflogo" id="logo" />
+                <img src="https://www.dropbox.com/s/1slj6dvgtwi0boz/off.png?raw=1" alt="off" id="off"/>
             </div>
             </React.Fragment>
         );
