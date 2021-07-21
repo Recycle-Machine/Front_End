377ec32a318e1c4c8662d631f7804efc8340c2d4
LuisSigala <a6519150031@utch.edu.mx>
2021-07-21 13:19:31 -0600

    Login design modified

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,26 +7,30 @@ class Login extends React.Component{
     render(){
         return(
             <React.Fragment>
+                <div className="loginBackground" style={{ height: '100vh' }}>
                     <center>
                         <Navbar></Navbar>
-                        <div className="Texts">
-                            <img className="imgLog" src={"https://cdn.iconscout.com/icon/free/png-256/account-avatar-profile-human-man-user-30448.png"} alt="" />
-                            <div className="fixedposition">
-                                <div className="form-group">
-                                    <input type="email" className="text" placeholder="Email"/>
+                        <div className="allItemsBackground">
+                            <div className="texts">
+                                <img className="imgLog" src={"https://cdn.iconscout.com/icon/free/png-256/account-avatar-profile-human-man-user-30448.png"} alt="" />
+                                <div className="fixedposition">
+                                    <div className="form-group">
+                                        <input type="email" className="text" placeholder="Email"/>
+                                    </div>
+                                    <br/>
+                                    <div className="form-group">
+                                        <input type="password" className="text" placeholder="Password"/>
+                                    </div>
+                                    </div>
+                                    <div>
                                 </div>
-                                <br/>
-                                <div className="form-group">
-                                    <input type="password" className="text" placeholder="Password"/>
-                                </div>
-                                </div>
-                                <div>
+                                    <Link to="/Landing">
+                                    <button className="button1">Login</button>
+                                    </Link>  
                             </div>
-                                <Link to="/Landing">
-                                <button className="button1">Login</button>
-                                </Link>  
                         </div>
                     </center>
+                </div>
             </React.Fragment>
         );
     }
