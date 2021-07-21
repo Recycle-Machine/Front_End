299b96c3fcfec9f953b31ad304be7f1cac583152
LuisSigala <a6519150031@utch.edu.mx>
2021-07-21 13:03:07 -0600

    Recycle Transactions Link modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import{ BrowserRouter, Route, Switch} from "react-router-dom"
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
+import Logout from './Pages/Logout/Logout';
 import Landing from './Pages/Landing/Landing';
 import Storage from './Pages/Storage/Storage';
 import NotFound from './Pages/NotFound/NotFound';
+import RecycleTransactions from './Pages/RecycleTransactions/RecycleTransactions';
 
 function App() {
   return (
@@ -12,8 +14,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/Login" component={Login}></Route>
+          <Route exact path="/Logout" component={Logout}></Route>
           <Route exact path="/Landing" component={Landing}></Route>
           <Route exact path="/Storage" component={Storage}></Route>
+          <Route exact path="/Recycle Transactions" component={RecycleTransactions}></Route>
           <Route component={NotFound}></Route>
         </Switch>
     </BrowserRouter>
