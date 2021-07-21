50c7d45ad2e7ebaad01f69f7c02a838fc79fb9a9
LuisSigala <a6519150031@utch.edu.mx>
2021-07-21 13:24:19 -0600

    Main design modified

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -6,13 +6,14 @@ class Main extends React.Component{
     render(){
         return(
             <React.Fragment>
+                <div className="mainBackground" style={{ height: '100vh' }}></div>
                 <div className="background">
                     <div>
                         <div className="position">
                             <br/><br/><br/><br/><br/><br/>
                             <center>
                                 <h1 className="title">Welcome</h1>
-                                <img className="img" src={"https://thumbs.dreamstime.com/b/mountain-lake-camp-ecological-landscape-flat-design-national-park-wildlife-sanctuary-scene-summer-camping-vector-minimal-style-68244632.jpg"} alt="" />
+                                <img className="img" src={"https://www.dropbox.com/s/4q1uxrp3pcpzwtf/Ecosystem-rafiki.svg?raw=1"} alt="" />
                                 <Link to="/Login">
                                 <button className="button">Save the world!</button>
                                 </Link>
