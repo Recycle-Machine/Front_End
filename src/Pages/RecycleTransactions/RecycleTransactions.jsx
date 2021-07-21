e76ce24e22bd422015242552d803edf64b544635
LuisSigala <a6519150031@utch.edu.mx>
2021-07-21 13:02:05 -0600

    Recycle Transactions desing and link it

---
+++ src/Pages/RecycleTransactions/RecycleTransactions.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+import Navbar from '../../components/Navbar';
+import "./RecycleTransactions.css"
+
+class RecycleTransactions extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+            <div className="transactionsBackground" style={{ height: '100vh' }}>
+                <Navbar></Navbar>
+                    <div className="bar">
+                        <h1 className="qr">QR</h1>        
+                        <h1 className="id">ID</h1>        
+                        <h1 className="machine">Machine</h1>        
+                        <h1 className="amount">Amount</h1>        
+                        <h1 className="details">Details</h1>        
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+                    <div className="barInfo">
+                        <img className="qrInfo" src={"https://www.dropbox.com/s/rydgeh5iv0mjgkh/icons8-codigo-qr-80.png?raw=1"} alt="" />
+                        <h1 className="idInfo">259A74</h1>        
+                        <h1 className="machineInfo">RC-01</h1>        
+                        <h1 className="amountInfo">16:46 - 09/21</h1>
+                        <img className="detailsInfo" src={"https://www.dropbox.com/s/lsqzok8b49y3f9h/icons8-mas-384.png?raw=1"} alt="" />
+                        <img className="deleteInfo" src={"https://www.dropbox.com/s/qqr98r1day6zh3q/icons8-basura-256.png?raw=1"} alt="" />  
+                    </div>
+            </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default RecycleTransactions;
