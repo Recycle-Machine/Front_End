1e2bbcdf4bdf89dd774aac2816f33f462f8dbf71
LuisSigala <a6519150031@utch.edu.mx>
2021-07-21 13:25:25 -0600

    NotFound design

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -6,7 +6,7 @@ class NotFound extends React.Component{
         return(
             <React.Fragment>
                 <center>            
-                    <img src="https://onlinezebra.com/wp-content/uploads/2019/01/error-404-not-found.jpg" alt="" className="notfound"/>
+                    <img className="notFound" src="https://onlinezebra.com/wp-content/uploads/2019/01/error-404-not-found.jpg" alt="" />
                 </center> 
             </React.Fragment>
         );
