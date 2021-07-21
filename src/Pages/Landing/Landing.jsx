a7b79d9c116ed92ae6e146aeeeeeba59ca13db53
LuisSigala <a6519150031@utch.edu.mx>
2021-07-21 13:18:19 -0600

    Landing design modified

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -7,23 +7,28 @@ class Landing extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <Navbar></Navbar>
-                        <div className="column">
-                            <div class="Texts">
-                                <img className="imgLan" src={"https://cdn.iconscout.com/icon/free/png-256/account-avatar-profile-human-man-user-30448.png"} alt="" />
-                                    <div className="buttonsposition">
-                                        <Link to="/MaterialsInformation">
-                                        <button className="buttonLan">Materials information</button>/button>
-                                        </Link>  
-                                        <Link to="/Storage">
-                                        <button className="buttonLan1">Storage</button>
-                                        </Link>  
-                                        <Link to="/Recycle Transactions">
-                                        <button className="buttonLan2">Recycle Transactions</button>
-                                        </Link>
-                                    </div>
+                <div className="landingBackground" style={{ height: '100vh' }}>
+                    <Navbar></Navbar>
+                        <div className="contentBackground">
+                            <div class="textsLan">
+                                <div className="imagesPosition">
+                                    <img className="imgLan" src={"https://cdn.iconscout.com/icon/free/png-256/account-avatar-profile-human-man-user-30448.png"} alt="" />
+                                    <img className="imgLan1" src={"https://www.dropbox.com/s/zar83qgkkupd86i/Jellyfish-amico.svg?raw=1"} alt="" />
+                                </div>
+                                <div className="buttonsPosition">
+                                    <Link to="/MaterialsInformation">
+                                    <button className="buttonLan">Materials information</button>
+                                    </Link>  
+                                    <Link to="/Storage">
+                                    <button className="buttonLan1">Storage</button>
+                                    </Link>  
+                                    <Link to="/Recycle Transactions">
+                                    <button className="buttonLan2">Recycle Transactions</button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
+                </div>
             </React.Fragment>
         );
     }
