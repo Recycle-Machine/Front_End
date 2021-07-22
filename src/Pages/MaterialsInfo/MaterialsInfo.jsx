4969335f68a5fed12eafed80ffaa0df18e706a7f
LuisSigala <a6519150031@utch.edu.mx>
2021-07-22 15:38:01 -0600

    MaterialsInfo css design change

--- src/Pages/MaterialsInfo/MaterialsInfo.jsx
+++ src/Pages/MaterialsInfo/MaterialsInfo.jsx
@@ -1,30 +1,26 @@
 import React from "react"
+import Navbar from "../../components/Navbar";
 import "./MaterialsInfo.css"
 
 class MaterialsInfo extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <div className="row">
-                    <div className="col-4">
-                        <div className="Card">
+                <div className="landingBackground" style={{ height: '100vh' }}>
+                <Navbar></Navbar>
+                <div id="row">
+                        <div className="CardInfo">
                             <div className="img_container"><img src="https://www.dropbox.com/s/ewr3ngkalfox4lx/icons8-bote-de-cerveza-50.png?raw=1" alt="Material" /></div>
                             <br></br>
                             <div className="material_title">Soda Cans</div>
                             <br></br>
                             <div className="points_text">Points per unit</div>
                             <br></br>
-                            <div className="points_container"><p>35</p></div>
+                            <div className="points_container">35</div>
                             <br></br>
                         </div>
-                    </div>
-                    <div className="col-6">
-                        <div className="Suggestions_container">
+                        <div id="Suggestions_container">
                             <div className="label"><p>Suggestions</p></div>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
                             <div className="text_field"><p></p></div>
                         <br />
                             <div className="text_field"><p></p></div>
@@ -32,7 +28,7 @@ class MaterialsInfo extends React.Component{
                             <div className="text_field"><p></p></div>
                         <br />
                     </div>
-                    </div>
+                </div>
                 </div>
             </React.Fragment>
         );
