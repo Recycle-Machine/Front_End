2c3730bb89fe2278a3e182a1a3b4b75fbd1507ed
LuisSigala <a6519150031@utch.edu.mx>
2021-07-22 11:58:47 -0600

    UserLanding link and links fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react' 
-import{ BrowserRouter, Route, Switch} from "react-router-dom"
+import { BrowserRouter, Route, Switch} from "react-router-dom"
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
 import Logout from './Pages/Logout/Logout';
 import Landing from './Pages/Landing/Landing';
 import Storage from './Pages/Storage/Storage';
 import NotFound from './Pages/NotFound/NotFound';
+import RecycleUser from './UserPages/Recycle/Recycle';
+import UserLanding from './UserPages/Landing/UserLanding';
 import MaterialsInfo from './Pages/MaterialsInfo/MaterialsInfo';
-import RecycleUser from './AdminPages/Recycle/Recycle';
 import RecycleTransactions from './Pages/RecycleTransactions/RecycleTransactions';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/Landing" component={Landing}></Route>
           <Route exact path="/Storage" component={Storage}></Route>
           <Route exact path="/Recycle" component={RecycleUser}></Route>
+          <Route exact path="/UserLanding" component={UserLanding}></Route>
           <Route exact path="/Materials information" component={MaterialsInfo}></Route>
           <Route exact path="/Recycle Transactions" component={RecycleTransactions}></Route>
           <Route component={NotFound}></Route>
