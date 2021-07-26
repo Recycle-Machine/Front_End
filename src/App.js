e74d18c24c1b44d69647ed197b5a64841e73a81b
LuisSigala <a6519150031@utch.edu.mx>
2021-07-26 14:36:01 -0600

    links change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react' 
 import { BrowserRouter, Route, Switch} from "react-router-dom"
 import QR from './UserPages/Qr/Qr';
-import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
 import Logout from './Pages/Logout/Logout';
 import Landing from './Pages/Landing/Landing';
@@ -15,25 +14,30 @@ import MaterialsInfo from './Pages/MaterialsInfo/MaterialsInfo';
 import MaterialsIndex from './Pages/MaterialsIndex/MaterialsIndex';
 import CanMaterialInfo from './Pages/CanMaterialInfo/CanMaterialInfo';
 import RecycleTransactions from './Pages/RecycleTransactions/RecycleTransactions';
+import UserMaterialsIndex from './UserPages/UserMaterialsIndex/UserMaterialsIndex';
+import UserCanMaterialInfo from './UserPages/UserCanMaterialInfo/UserCanMaterialInfo';
+import UserBottleMaterialInfo from './UserPages/UserBottleMaterialInfo/UserBottleMaterialInfo';
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Main}></Route>
           <Route exact path="/QR" component={QR}></Route>
+          <Route exact path="/" component={UserMain}></Route>
           <Route exact path="/Login" component={Login}></Route>
           <Route exact path="/Logout" component={Logout}></Route>
           <Route exact path="/Landing" component={Landing}></Route>
           <Route exact path="/Storage" component={Storage}></Route>
           <Route exact path="/Rewards" component={Rewards}></Route>
-          <Route exact path="/UserMain" component={UserMain}></Route>
           <Route exact path="/Recycle" component={RecycleUser}></Route>
           <Route exact path="/UserLanding" component={UserLanding}></Route>
           <Route exact path="/Materials" component={MaterialsIndex}></Route>
-          <Route exact path="/Bottle information" component={MaterialsInfo}></Route>
           <Route exact path="/Can information" component={CanMaterialInfo}></Route>
+          <Route exact path="/Bottle information" component={MaterialsInfo}></Route>
+          <Route exact path="/User Materials" component={UserMaterialsIndex}></Route>
+          <Route exact path="/User Can information" component={UserCanMaterialInfo}></Route>
           <Route exact path="/Recycle Transactions" component={RecycleTransactions}></Route>
+          <Route exact path="/User bottle information" component={UserBottleMaterialInfo}></Route>
           <Route component={NotFound}></Route>
         </Switch>
     </BrowserRouter>
